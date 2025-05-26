46c7bf7c9c9ce1e61ceb385348b1d384eafced87
think <lingwh.walkman@hotmail.com>
2025-05-26 16:09:48 +0800

    修改项目结构

--- 027_关键字-static-生命周期和作用域/src/mylib.h
+++ 027_关键字-static-生命周期和作用域/src/mylib.h
@@ -1,11 +1,11 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 //声明其他文件中的一个被static修饰的全局变量
-static extern int gi_in_other_file_static;
+//static extern int gi_in_other_file_static;
 //声明其他文件中的一个被static修饰的全局常量
-static extern const int gc_in_other_file_static;
+//static extern const int gc_in_other_file_static;
 //声明其他文件中的一个被static修饰的方法
-static extern int AddInOtherFileStatic(int, int);
+//static extern int AddInOtherFileStatic(int, int);
 
 //声明其他文件中的一个没有被static修饰的全局变量
 extern int gi_in_other_file_ordinary;
